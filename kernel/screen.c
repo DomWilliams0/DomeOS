d49b58231d842722629084631e41b73efbcccd01
Dom Williams <me@domwillia.ms>
2016-09-19 22:25:12 +0200

    Add screen printing skeleton

---
+++ kernel/screen.c
@@ -0,0 +1,31 @@
+#include "util/util.h"
+#include "screen.h"
+
+
+RESULT write_char(int x, int y, char c, screen_colour colour)
+{
+	RESULT result = RESULT_FAILURE;
+
+	UNUSED(x);
+	UNUSED(y);
+	UNUSED(c);
+	UNUSED(colour);
+
+	// TODO actually do something here
+
+	return result;
+}
+
+RESULT write_string(int x, int y, char *s, screen_colour colour)
+{
+	RESULT result = RESULT_FAILURE;
+
+	UNUSED(x);
+	UNUSED(y);
+	UNUSED(s);
+	UNUSED(colour);
+
+	// TODO here too
+
+	return result;
+}
--- kernel/util/err.h
+++ kernel/util/err.h
@@ -1,7 +1,7 @@
 #ifndef __KERNEL_ERR_H__
 #define __KERNEL_ERR_H__
 
-typedef RESULT unsigned int
+typedef unsigned int RESULT;
 
 #define RESULT_SUCCESS		0x0000
 #define RESULT_FAILURE		0xFFFF
