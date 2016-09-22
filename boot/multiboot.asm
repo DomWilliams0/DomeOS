a3b42ca25d9007ad064b6c46024a69140705e20c
Dom Williams <me@domwillia.ms>
2016-09-22 10:40:01 +0200

    Fix assembly formatting

--- boot/multiboot.asm
+++ boot/multiboot.asm
@@ -17,16 +17,16 @@ CHECKSUM    equ -(MAGIC + MB_FLAGS)
 ; multiboot header
 section .multiboot
 align   4
-    dd MAGIC
-    dd MB_FLAGS
-    dd CHECKSUM
+	dd MAGIC
+	dd MB_FLAGS
+	dd CHECKSUM
 
 
 ; allocate petit stack of 16KiB
 section .bss
 align   4
 stack_bottom:
-resb 16384
+	resb 16384
 stack_top:
 
 section .text
@@ -35,34 +35,34 @@ section .text
 global  gdt_flush
 extern  gdt_descriptor
 gdt_flush:
-lgdt    [gdt_descriptor]
+	lgdt    [gdt_descriptor]
 
-; 0x10 = 16 = 2nd entry = data segment
-mov     ax, 0x10
-mov     ds, ax
-mov     es, ax
-mov     fs, ax
-mov     gs, ax
-mov     ss, ax
+	; 0x10 = 16 = 2nd entry = data segment
+	mov     ax, 0x10
+	mov     ds, ax
+	mov     es, ax
+	mov     fs, ax
+	mov     gs, ax
+	mov     ss, ax
 
-; 0x08 = 08 = 1st entry = code segment
-jmp     0x08:flush_far_jump
+	; 0x08 = 08 = 1st entry = code segment
+	jmp     0x08:flush_far_jump
 
 flush_far_jump:
-    ret
+	ret
 
 ; entry point
-global _start:function (_start.end - _start)
+global  _start:function (_start.end - _start)
 _start:
 
 ; setup stack
 mov     esp, stack_top
 
-; boot kernel
+; run kernel
 extern  kernel_main
 call    kernel_main
 
-; hang
+; hang on exit
 cli
 jmp     $
 
