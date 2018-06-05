eb21af7359ad4a947d3b093edc202afb1560bac4
Dom Williams <me@domwillia.ms>
2018-06-05 14:16:53 +0100

    Add logging of registers on exception

--- src/interrupts.c
+++ src/interrupts.c
@@ -40,6 +40,35 @@ static char *exceptions[] =
 	"Reserved",                    // 31
 };
 
+static void log_registers(struct intr_context *ctx) {
+	printf("%s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"\n%s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"\n%s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"\n%s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"\n%s: 0x%x"
+			"  %s: 0x%x"
+			"  %s: 0x%x"
+			"\n"
+			, "rax", ctx->rax, "rbx", ctx->rbx, "rcx", ctx->rcx
+			, "rdx", ctx->rdx, "rsi", ctx->rsi, "rbp", ctx->rbp
+			, "r8", ctx->r8, "r9", ctx->r9, "r10", ctx->r10
+			, "r11", ctx->r11, "r12", ctx->r12, "r13", ctx->r13
+			, "r14", ctx->r14, "r15", ctx->r15, "rip", ctx->rip
+			, "rflags", ctx->rflags, "rsp", ctx->rsp, "ss", ctx->ss
+			);
+}
+
 
 // log and never return
 // TODO print all registers
@@ -50,6 +79,7 @@ void fault_handler(struct intr_context *ctx)
 	  printf("\n=======\n");
 	  screen_set_colours(SCREEN_COLOUR_WHITE, SCREEN_COLOUR_RED);
 	  printf("Unhandled exception %d: %s\nHalting\n", ctx->int_no, exceptions[ctx->int_no]);
+	  log_registers(ctx);
 
 	  disable_interrupts();
 	  while(1);
