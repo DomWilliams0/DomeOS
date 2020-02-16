efd23d89a0e4e83144c198ffd9c47cbbd042eb97
Dom Williams <me@domwillia.ms>
2020-02-16 19:15:44 +0000

    #7: Discover HDD sector size and print with newlines

--- boot/stage0/stage0.asm
+++ boot/stage0/stage0.asm
@@ -5,51 +5,79 @@ stage0:
 
 ; OEM parameter block - for fs reading
 
-bpbOEM                db "DomeOS  "
-bpbBytesPerSector:    dw 512
-bpbSectorsPerCluster: db 1
-bpbReservedSectors:   dw 1
-bpbNumberOfFATs:      db 2
-bpbRootEntries:       dw 224
-bpbTotalSectors:      dw 2880
-bpbMedia:             db 0xF0
-bpbSectorsPerFAT:     dw 9
-bpbSectorsPerTrack:   dw 18
-bpbHeadsPerCylinder:  dw 2
-bpbHiddenSectors:     dd 0
-bpbTotalSectorsBig:   dd 0
-bsDriveNumber:        db 0
-bsUnused:             db 0
-bsExtBootSignature:   db 0x29
-bsSerialNumber:       dd 0xa0a1a2a3
-bsVolumeLabel:        db "MOS      FLOPPY "
-bsFileSystem:         db "FAT12    "
-
 s_booting    db    "[+] Booting DomeOS...", 0
+s_error      db    "[!] Fatal error", 0
+s_sectorsz   db    "[+] Sector size is not 52?!", 0
+s_newline    db    0x0d, 0x0a, 0
 
 ; ds=>si: nul terminated string
 print:
-    lodsb
-    or      al, al  ; al=current character
-    jz      .done   ; nul terminator found
-    mov     ah, 0eh ; for int 10h -> get next character
+    mov     bh, 1      ; new line flag set initially
+.loop:
+    lodsb              ; load ds:si into al and increment si
+    or      al, al     ; al=current character
+    jz      .newline   ; nul terminator found
+    mov     ah, 0eh    ; for int 10h -> get next character
     int     10h
-    jmp     print  ; loop
-.done:
-    ret
+    jmp     .loop      ; loop
+
+.newline:
+    cmp     bh, 1
+    je     .newline_prep ; if bh is 1, loop again with a newline
+    ret                   ; otherwise its 0 so just return
+
+.newline_prep:
+    xor     bh, bh ; zero bh so next loop will ret
+    mov     si, s_newline
+    jmp     .loop
+
 
 loader:
-    xor    ax, ax ; null data secments because theyre in the same 0x7c00:0 range as code
+    cli
+
+    ; TODO preserve DL, its the drive number
+    xor    ax, ax ; null data segments because theyre in the same 0x7c00:0 range as code
     mov    ds, ax
     mov    es, ax
 
     mov    si, s_booting
     call   print
+    mov     si, ds
 
     ; TODO get available memory
 
-    cli   ; disable interrupts
-    jmp $ ; loop forever
+    ; TODO check if supported with ah=41h
+
+    ; get drive params
+    mov     ax, 7e0h
+    mov     ds, ax ; use 0x50:0 as data segment (from 0x500 physical)
+    mov     byte [0], 1eh ; length
+    mov     ah, 48h ; for int 13h -> extended read drive params, leave dl as it is
+    int     13h
+    jc fatal_error
+
+    ; verify sector size == 512 TODO use this value instead of 512
+    mov     ax, [18h]
+    cmp     ax, 512
+    jnz     .bad_sector_size
+
+    jmp $
+
+.bad_sector_size:
+    xor     ax, ax
+    mov     ds, ax
+    mov     si, s_sectorsz
+    call    print
+    ret
+
+
+fatal_error:
+    cli
+    xor     ax, ax
+    mov     ds, ax
+    mov     si, s_error
+    call    print
+    hlt
 
 times 510 - ($-$$) db 0 ; pad to 512 bytes
 
